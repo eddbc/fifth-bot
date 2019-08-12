14ddc179027ffdb5206b39ca873abb42ba26efad
Edward Billington-Cliff <yddebc@gmail.com>
2019-08-12 12:32:00 +0100

    circlejerk

--- fifth/cron.go
+++ fifth/cron.go
@@ -15,11 +15,11 @@ func init() {
 
 	C.AddFunc("@every 10s", func() { timerCron() })
 	//C.AddFunc("@every 3m", func(){ getAllContracts() })
-	//C.AddFunc("0 0 19 * * *", func() { circlejerk() })
+	C.AddFunc("0 0 19 * * *", func() { circlejerk() })
 
 	C.Start()
 }
 
 func circlejerk() {
-	//SendImportantMsg("Daily reminder: Don't forget to circlejerk! <https://www.pandemic-legion.pl/forums/topic/16750-circlejerk/?do=getNewComment>")
+	SendImportantMsg("Daily reminder: Don't forget to circlejerk! <https://forums.nog8s.space/topic/3-circlejerk/?do=getNewComment>")
 }
